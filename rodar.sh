0843d30fc547fc8b8b5eb34143062b0b7dcd4a7f
Júlia Rodrigues <105869092+juliarmn@users.noreply.github.com>
2024-10-17 00:39:20 -0300

    Update rodar.sh

--- rodar.sh
+++ rodar.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+
+THREADS=(1 2 4 8)
+INTERVALS_X=(1000 10000 100000)
+INTERVALS_Y=(1000 10000 100000)
+
+for threads in "${THREADS[@]}"; do
+    for n_intervals_x in "${INTERVALS_X[@]}"; do
+        for n_intervals_y in "${INTERVALS_Y[@]}"; do
+            echo "Executando com OMP_NUM_THREADS=${threads}, n_intervals_x=${n_intervals_x}, n_intervals_y=${n_intervals_y}"
+            OMP_NUM_THREADS=$threads ./p $n_intervals_x $n_intervals_y
+        done
+    done
+done
 
